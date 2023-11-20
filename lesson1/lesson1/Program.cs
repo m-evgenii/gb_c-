15fac9e019d16f47c092902fa38e2c1a104fb9d3
evgenii <matazov2009@gmail.com>
2023-11-20 13:26:15 +0300

    first commit

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+static void gig()
+{
+    int furst_number = Convert.ToInt32(Console.ReadLine());
+    string symvol = Console.ReadLine();
+    int second_number = Convert.ToInt32(Console.ReadLine());
+    if (symvol == "+")
+    {
+        Console.WriteLine(furst_number + second_number);
+    }
+    else if (symvol == "-")
+    {
+        Console.WriteLine(furst_number - second_number);
+    }
+    else if (symvol == "*")
+    {
+        Console.WriteLine(furst_number * second_number);
+    }
+    else if (symvol == "/")
+    {
+        if (second_number == 0)
+        {
+            Console.WriteLine("Ошибка! Нелзя делить на ноль!");
+        }
+        else
+        {
+            Console.WriteLine(furst_number / second_number);
+        }
+    }
+}
+gig();
